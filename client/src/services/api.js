decb7cdf919d5dff35c2a22299af057249850035
Anshika204 <122024756+Anshika204@users.noreply.github.com>
2026-02-23 12:57:41 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://blog-website-e6r3.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
